887aacc94ff98cd8479839ab8f677110057f5c29
Mamad <m.pooladkhay@gmail.com>
2020-10-29 17:59:46 +0330

    update server

--- udp/ping-server.js
+++ udp/ping-server.js
@@ -5,11 +5,11 @@ const server = dgram.createSocket("udp4");
 const host = process.env.ADDR;
 const port = process.env.UDP_PORT;
 
-let count = 0;
+// let count = 0;
 
 server.on("message", (msg, rinfo) => {
 	server.send(msg, 0, msg.length, rinfo.port, rinfo.address);
-	console.log(count++ + " <--> " + rinfo.address + ":" + rinfo.port);
+	// console.log(count++ + " <--> " + rinfo.address + ":" + rinfo.port);
 });
 
 server.bind(port, host, () => {
